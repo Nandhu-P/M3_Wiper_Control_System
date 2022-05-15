76ffb89375ec6df28653a2406622ce89ecf4c159
Nandhu-P <102171569+Nandhu-P@users.noreply.github.com>
2022-05-15 16:24:05 +0530

    Update wiper.c

--- 3_Implementation/src/wiper.c
+++ 3_Implementation/src/wiper.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-uint32_t TARGET = 0;
+uint32_t GOAL = 0;
 uint32_t BUTTON_ON = 0;
 uint32_t BUTTON_OFF =0;
 uint32_t LED= 0 ;
@@ -18,30 +18,30 @@ GPIO_OUT(GPIOA, GPIO_PIN_NO_0, DISABLE);
 if (GPIO_IN(GPIOA, GPIO_PIN_NO_0) == ENABLE)
 {
 			Delay_time();
-			TARGET++;
-			if ((TARGET == 1) && (BUTTON_ON != 1) && (BUTTON_ON != 2) && BUTTON_ON != 3)
+			GOAL++;
+			if ((GOAL == 1) && (BUTTON_ON != 1) && (BUTTON_ON != 2) && BUTTON_ON != 3)
 			{
 				on_key();
-				BUTTON_ON = TARGET;
-				TARGET = 0;
+				BUTTON_ON = GOAL;
+				GOAL = 0;
 			}
-			else if ((TARGET == 2) && (BUTTON_ON != 2) && BUTTON_ON != 3)
+			else if ((GOAL == 2) && (BUTTON_ON != 2) && BUTTON_ON != 3)
 			{
 				off_key();
-				BUTTON_ON = TARGET;
-				TARGET = 0;
+				BUTTON_ON = GOAL;
+				GOAL = 0;
 			}
-			else ((ifTARGET == 3) && (BUTTON_ON != 3))
+			else ((ifGOAL == 3) && (BUTTON_ON != 3))
 			{
 				clockwise_motion();
-				BUTTON_ON = TARGET;
-				TARGET = 0;
+				BUTTON_ON = GOAL;
+				GOAL = 0;
 			}
-			else if (TARGET == 4)
+			else if (GOAL == 4)
 			{
 				off_wiper();
-				BUTTON_ON = TARGET;
-				TARGET = 0;
+				BUTTON_ON = GOAL;
+				GOAL = 0;
 			}
 }
 }
